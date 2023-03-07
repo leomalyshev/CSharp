ddb2b6a34a559664d229385706b15f16d9eb9c25
Leonid Malyshev <lokiwish@list.ru>
2023-03-08 00:36:04 +0300

    init

---
+++ Learn/Introduction/Seminars/Second/5 task/Program.cs
@@ -0,0 +1,13 @@
+﻿int x = Convert.ToInt32(Console.ReadLine());
+int result = (int)Math.Floor(Math.Log(x, 10) + 1);
+
+if (result>1)
+{
+    int digit = x/(int)Math.Pow(10, result-2);
+    Console.Write(digit%10);
+}
+else Console.WriteLine("Sorry, dude");
+
+
+
+
