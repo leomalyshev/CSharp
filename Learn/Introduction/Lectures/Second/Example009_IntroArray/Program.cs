2d9b18d92a354f53aacd9c3ca35465ddf3987a7c
Leonid Malyshev <lokiwish@list.ru>
2023-03-08 20:02:05 +0300

    New task

--- Learn/Introduction/Lectures/Second/Example009_IntroArray/Program.cs
+++ Learn/Introduction/Lectures/Second/Example009_IntroArray/Program.cs
@@ -5,34 +5,14 @@
     if(arg3>result) result = arg3;
     return result;
 }
+//              0   1   2   3   4   5   6   7   8
+int[] array = {11, 12, 13, 44, 35, 1633, 17, 18, 19};
+array[0] = 12;
+Console.WriteLine(array[4]);
 
-int a1 = 15;
-int b1 = 21;
-int c1 = 39;
-int a2 = 12;
-int b2 = 23;
-int c2 = 33;
-int a3 = 13;
-int b3 = 23;
-int c3 = 333;
-
-// int max = a1;
-// if(b1>max) max = b1;
-// if(c1>max) max = c1;
-// if(a2>max) max = a2;
-// if(b2>max) max = b2;
-// if(c2>max) max = c2;
-// if(a3>max) max = a3;
-// if(b3>max) max = b3;
-// if(c3>max) max = c3;
-
-// int max1 = Max(a1, b1, c1);
-// int max2 = Max(a2, b2, c2);
-// int max3 = Max(a3, b3, c3);
-
-int max = Max(
-    Max(a1, b1, c1), 
-    Max(a2, b2, c2), 
-    Max(a3, b3, c3));
-
-Console.WriteLine(max);
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
