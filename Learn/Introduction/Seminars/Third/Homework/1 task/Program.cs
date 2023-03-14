b209f5ca20165d1ee9600522ad0849c326d78fe3
Leonid Malyshev <lokiwish@list.ru>
2023-03-14 23:34:39 +0300

    third homework

---
+++ Learn/Introduction/Seminars/Third/Homework/1 task/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом. Через строку решать нельзя.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int reverseNum = reverseNumber(number);
+Console.WriteLine(checkPalindrome(number, reverseNum));
+
+
+
+
+int reverseNumber(int number)
+{
+    int reverseNumber = 0;
+    while (number > 0)
+    {
+        reverseNumber = reverseNumber * 10 + number % 10;
+        number /= 10;
+    }
+
+    return reverseNumber;
+}
+
+string checkPalindrome(int number, int reverseNumber)
+{
+    if (number == reverseNumber)
+        return "Да";
+    return "Нет";
+}
---
+++ Learn/Introduction/Seminars/Third/Seminar/3 task/Program.cs
@@ -0,0 +1,51 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+/* Напишите программу, которая принимает на вход координаты двух точек и 
+находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09 
+A (7,-5); B (1,-1) -> 7,21
+
+/* Console.WriteLine("Введите x 1 точки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y 1 точки");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите x 2 точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y 2 точки");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(DistancePount(x1, y1, x2, y2));
+
+
+
+
+double DistancePount(int x1, int y1, int x2, int y2)
+{
+    double result1 = (x2 - x1)*(x2 - x1);
+    double result2 = (y2 - y1)*(y2 - y1);
+    double result = Math.Sqrt(result1 + result2);
+    return result;
+} */
+
+//√((x2 - x1)² + (y2 - y1)²)
+
+/*  Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4 */
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write($"{i * i} ");
+}
