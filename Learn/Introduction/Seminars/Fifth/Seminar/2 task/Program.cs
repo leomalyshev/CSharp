f27af14463f6b6eeb1b3fce08f511aee28d485c1
Leonid Malyshev <lokiwish@list.ru>
2023-03-22 00:24:11 +0300

    add fifth

---
+++ Learn/Introduction/Seminars/Fifth/Seminar/2 task/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+// int[] array = new int[4];
+// FillArray(array);
+// PrintArray(array);
+// //Reverse2(array);
+// Reverse(array);
+// PrintArray(array);
+
+
+// void FillArray(int[] array)
+// {
+//     for (int i = 0; i  < array.Length; i++)
+//         array[i] = new Random().Next(-9, 10);
+// }
+
+// void PrintArray(int[] array)
+// {
+//     foreach (int item in array)
+//     {
+//         Console.Write($"{item} ");
+//     }
+//     Console.WriteLine();
+// }
+// int[] Reverse(int[] array)
+// {
+//     for (int i = 0; i  < array.Length; i++)
+//     {
+//         array[i]= -array[i];
+//     }
+//     return array;
+// }
