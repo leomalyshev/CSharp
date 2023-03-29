d77845386c5a5c648bc710d56e801aeaf975baa0
Leonid Malyshev <lokiwish@list.ru>
2023-03-29 23:21:09 +0300

    add new tasks

---
+++ Learn/Introduction/Seminars/Sixth/Seminar/1 task/Program.cs
@@ -0,0 +1,31 @@
+﻿void FillArray(int[] array)
+{
+    for (int i = 0; i  < array.Length; i++)
+        array[i] = new Random().Next(0,10);
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+
+void ReverseArray(int[] array)
+{
+    for (int i = 0; i  < array.Length/2; i++)
+        (array[i], array[array.Length - 1 - i]) = (array[array.Length - 1 - i], array[i]);
+        // int temp = array[i];
+        // array[i] = array[array.Length - 1 - i];
+        // array[array.Length - 1 - i] = temp;
+}
+
+Console.WriteLine($"Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [size];
+FillArray(array);
+PrintArray(array);
+ReverseArray(array);
+PrintArray(array);
