cfaa8dbfbeb208f8d5c6a075bf57d14ae0be2c12
Leonid Malyshev <lokiwish@list.ru>
2023-04-04 22:41:23 +0300

    add new tasks

---
+++ Learn/Introduction/Seminars/Ningth/Seminar/1 task/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите натуральное число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Искомая сумма равна {Sum(num)}");
+Console.WriteLine($"Искомая сумма равна {Sum1(5)}");
+
+
+
+
+int Sum(int num)
+{
+    int sum = 0;
+    while(true)
+    {
+        if (num==0) break;
+        sum+=num;
+        num--;
+    }
+    return sum;
+}
+
+int Sum1(int num)
+{
+    if (num==0) return 0;
+    return num + Sum1(num-1);
+}
+
+// Sum1(5)
+// 5 + (4 + (3 + 2 + (1 + 0)))
