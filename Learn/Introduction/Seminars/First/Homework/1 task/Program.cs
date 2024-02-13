4c26f9b386381d7e1e6407587d7e0547794e649c
Leonid Malyshev <lokiwish@list.ru>
2024-02-13 23:15:30 +0300

    add homework

--- Learn/Introduction/Seminars/First/Homework/1 task/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿int num = Convert.ToInt32(Console.ReadLine());
-
-if (num%2==0) Console.WriteLine("Да");
-else Console.WriteLine("Нет");
--- Learn/Introduction/Seminars/First/Seminar/1 task/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
-
-if (a>b) Console.WriteLine(a);
-else Console.WriteLine(b);
