b445bb59fde181a94d3e3a76fb52e66c0d7c9b2f
Leonid Malyshev <lokiwish@list.ru>
2024-03-02 00:20:25 +0300

    add changes

---
+++ Learn/Geekbrains/Seminar3/Task5.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar3
+{
+    public class Task5
+    {
+        static void Main6(string[] args)
+        {
+            List<int> ints = new List<int> { 1, 2, 2, 2, 3, 4, 4, 5, 5, 5, 5, 6, 7, 0 };
+
+            Dictionary<int, int> upDictionary = new Dictionary<int, int>();
+
+            foreach (var i in ints)
+            {
+                if (upDictionary.ContainsKey(i)) upDictionary[i]++;
+                else
+                {
+                    upDictionary[i] = 1;
+                }
+            }
+
+            PriorityQueue<int, int> priorityQueue = new PriorityQueue<int, int>();
+
+
+            foreach (var upDict in upDictionary)
+            {
+                priorityQueue.Enqueue(upDict.Key,upDict.Value);
+            }
+
+
+            while (priorityQueue.Count > 0)
+            {
+                Console.WriteLine(priorityQueue.Dequeue());
+            }
+        }
+    }
+}
---
+++ Learn/Geekbrains/Seminar3/Task8.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar3
+{
+    internal class Task8
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 44, 5, 6, 7, 8, 9, 10, 11 };
+
+            int target = 6;
+
+            var s = new HashSet<int>();
+
+            int[] arr2 = { 12, 3, 4, 1, 6, 9 }; // 1, 3, 4, 6, 9, 12
+            int sum = 24;
+
+
+
+
+            foreach (var i in arr)
+            {
+                var x = target - i; // 5 = 6 - 1
+                if (arr.Contains(x))
+                {
+                    foreach (var j in arr)
+                    {
+                        var y = x - j; // 3 = 5 - 2
+                        if (arr.Contains(y) && target == y + i + j)
+                        {
+                            Console.WriteLine($"{y} + {i} + {j} = {target}");
+                            return;
+                        }
+
+                    }
+                }
+                
+            }
+        }
+    }
+}
