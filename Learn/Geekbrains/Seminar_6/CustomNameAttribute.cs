404350044558b61264304cbfdcad4a9e69d67ba5
Leonid Malyshev <lokiwish@list.ru>
2024-03-14 20:06:11 +0300

    add

---
+++ Learn/Geekbrains/Seminar_6/CustomNameAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar_6
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class CustomNameAttribute(string customName) : Attribute
+    {
+        public string CustomName { get; private set; } = customName;
+    }
+}
