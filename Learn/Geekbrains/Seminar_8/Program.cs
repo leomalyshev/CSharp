c029bc9b3b49e17877d2cb03031e6095875b5af0
Leonid Malyshev <lokiwish@list.ru>
2024-03-15 08:54:24 +0300

    add homework

---
+++ Learn/Geekbrains/Seminar_8/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Seminar_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Homework.SearchFile(args[0], args[1], args[2]);
+
+            foreach (var line in Homework.list)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Learn/Geekbrains/Seminar_8/Task2.cs
@@ -0,0 +1,77 @@
+﻿namespace Seminar_8
+{
+    internal class Task2
+    {
+        //private static List<string> list = new List<string>();
+        //public static void SearchFile(string path, string fileName)
+        //{
+        //    var files = Directory.GetFiles(path);
+
+        //    foreach (var file in files)
+        //    {
+        //        var name = Path.GetFileName(file);
+        //        if (name == fileName)
+        //        {
+        //            list.Add(file);
+        //        }
+        //    }
+
+        //    var directories = Directory.GetDirectories(path);
+        //    foreach (var directory in directories)
+        //    {
+        //        SearchFile(directory, fileName);
+        //    }
+        //}
+
+
+        static void Main2(string[] args)
+        {
+
+
+
+
+            //using (StreamReader sr = new StreamReader(args[0]))
+            //{
+            //    while (!sr.EndOfStream)
+            //    {
+            //        var tempString = sr.ReadLine();
+            //        if (tempString.Contains(args[1]))
+            //        {
+            //            Console.WriteLine(tempString);
+            //        }
+            //    }
+            //}
+
+            //SearchFile(args[0], args[1]);
+            //foreach (var l in list)
+            //{
+            //    Console.WriteLine(l);
+            //}
+
+            //if (args.Length < 2)
+            //{
+            //    Console.WriteLine("Нет параметров запуска");
+            //}
+            //else
+            //{
+            //    if (!File.Exists(args[0]))
+            //    {
+            //        using (StreamWriter sw = new StreamWriter(args[0]))
+            //        {
+            //            sw.WriteLine("Сгенерированная строка");
+            //        } ;
+            //    }
+            //    else
+            //    {
+            //        using (StreamWriter sw = new StreamWriter(args[1]))
+            //        {
+            //            using (StreamReader sr = new StreamReader(args[0]))
+            //            {
+            //                sw.Write(sr.ReadToEnd());
+            //            }
+            //        }
+            //    }
+            //}
+        }
+    }
+}
