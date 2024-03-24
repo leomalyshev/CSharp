bc02af77bb778f91998653ae5a950a572b76a1d3
Leonid Malyshev <lokiwish@list.ru>
2024-03-24 22:49:58 +0300

    add server

---
+++ Learn/Geekbrains/UdpApp/Message.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+namespace UdpApp
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        public DateTime DateTime { get; set; }
+        public string NicknameFrom { get; set; }
+        public string NicknameTo { get; set; }
+
+        public string SerialiseMessageToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public static Message? DeserializeFromJson(string message)
+        {
+            return JsonSerializer.Deserialize<Message>(message);
+        }
+
+
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public override string ToString() => $"{this.DateTime} получено сообщение {this.Text} от {this.NicknameFrom}";
+    }
+}
---
+++ Learn/Geekbrains/UdpApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Net.Security;
+using System.Text;
+
+namespace UdpApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server("Hello");
+        }
+
+        public void task1()
+        {
+            Message msg = new Message() { Text = "Hello", DateTime = DateTime.Now, NicknameFrom = "Leonid" };
+
+            string json = msg.SerialiseMessageToJson();
+            Console.WriteLine(json);
+            Message? msgDeserialized = Message.DeserializeFromJson(json);
+        }
+
+
+        public static void Server(string name)
+        {
+            UdpClient client = new UdpClient(12345);
+            IPEndPoint ep = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine("Сервер ждет сообщения от клиента");
+
+
+            while (true)
+            {
+                byte[] buffer = client.Receive(ref ep);
+                string message = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine("Сообщение получено");
+
+                ThreadPool.QueueUserWorkItem(obj =>
+                {
+                    Message? msg = Message.DeserializeFromJson(message);
+                    msg.Print();
+
+                    byte[] reply = "Сообщение доставлено"u8.ToArray();
+                    client.Send(reply, reply.Length, ep);
+                });
+            }
+        }
+    }
+}
