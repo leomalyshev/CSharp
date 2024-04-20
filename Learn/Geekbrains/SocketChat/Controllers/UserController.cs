79b484cff2b93e5ddf0d4f328223443f14fd29fd
Leonid Malyshev <lokiwish@list.ru>
2024-04-20 19:10:52 +0300

    Add SockerChat

---
+++ Learn/Geekbrains/SocketChat/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocketChat.BLL.Logic;
+using SocketChat.Common.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SocketChat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserLogic _userLogic;
+
+        public UserController(IUserLogic userLogic)
+        {
+            _userLogic = userLogic;
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public async Task<IEnumerable<User>> GetAsync()
+        {
+            return await _userLogic.GetAllAsync();
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] User user)
+        {
+            await _userLogic.AddAsync(user);
+            return Ok();
+        }
+
+    }
+}
